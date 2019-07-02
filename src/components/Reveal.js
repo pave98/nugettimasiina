a119650287b725a11d04a7a884e147b5aa9f5876
Paavo Porkka <ppporkka@tuni.fi>
2019-07-02 16:10:31 +0300

    Donesadadsa
    öpäååååååååååååå

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card'
 export default class Reveal extends Component {
   render() {
     return (
-      <Card className="CardReveal" style={{  }}>
+      <Card className='CardReveal' style={{}}>
         <Card.Body>
           In mommy’s belly I will grow
           <br />Tiny fingers and tiny toes
@@ -14,16 +14,6 @@ export default class Reveal extends Component {
           <br />I’m sure either way you will jump for joy!
           <br />Read this riddle with great care
           <br />Look really close and we will share!
-          <br />
-          ----------------------------------------
-          <br />
-          In the belly I will grow
-          <br />Tiny hands and tiny toes
-          <br />Secret will my gender be
-          <br />Although you might already see
-          <br />Between the lines the truth does lay
-          <br />On the surface confused you’ll stay
-          <br />You might be happy either way!
         </Card.Body>
       </Card>
     )
